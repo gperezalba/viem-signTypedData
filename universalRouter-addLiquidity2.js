7ce37ab5c4b95ac01cb7390cdac3b25eef6a0f8d
gperezalba <gperezalba94@gmail.com>
2024-10-23 17:09:00 +0200

    feat(all) addLiquidityWithPermit both tokens

---
+++ universalRouter-addLiquidity2.js
@@ -0,0 +1,126 @@
+import { createPublicClient, http, createWalletClient, getContract, parseSignature, verifyTypedData, parseUnits, encodeAbiParameters, getAbiItem, parseEther, zeroHash, maxUint256 } from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
+import { UNIVERSAL_ROUTER_ABI } from "./UNIVERSAL_ROUTER_ABI.js"
+import { GT3_ABI } from "./GT3_ABI.js"
+
+const MY_SEPOLIA_RPC = process.env.RPC
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS
+const GT3_ADDRESS = process.env.GT3_ADDRESS
+const WBTC_ADDRESS = process.env.WBTC_ADDRESS
+
+const client = createPublicClient({
+    chain: sepolia,
+    transport: http(MY_SEPOLIA_RPC),
+})
+
+const walletClient = createWalletClient({
+    account: privateKeyToAccount(PRIVATE_KEY),
+    transport: http(MY_SEPOLIA_RPC)
+})
+
+const gt3Contract = getContract({
+    address: GT3_ADDRESS,
+    abi: GT3_ABI,
+    client: client,
+})
+
+const wbtcContract = getContract({
+    address: WBTC_ADDRESS,
+    abi: GT3_ABI,
+    client: client,
+})
+
+const routerContract = getContract({
+    address: ROUTER_ADDRESS,
+    abi: UNIVERSAL_ROUTER_ABI,
+    client: client,
+})
+
+const domainA = {
+    name: "GT3 Token",
+    version: "1",
+    chainId: 11155111,
+    verifyingContract: GT3_ADDRESS,
+}
+
+const domainB = {
+    name: "Wrapped Bitcoin",
+    version: "1",
+    chainId: 11155111,
+    verifyingContract: WBTC_ADDRESS,
+}
+
+const types = {
+    Permit: [
+        { name: "owner", type: "address" },
+        { name: "spender", type: "address" },
+        { name: "value", type: "uint256" },
+        { name: "nonce", type: "uint256" },
+        { name: "deadline", type: "uint256" },
+    ]
+}
+
+async function main() {
+    const account = privateKeyToAccount(PRIVATE_KEY)
+    const userPermitNonceA = await gt3Contract.read.nonces([account.address])
+    const userPermitNonceB = await wbtcContract.read.nonces([account.address])
+
+    const messageA = {
+        owner: account.address,
+        spender: ROUTER_ADDRESS,
+        value: parseEther("1"),
+        nonce: userPermitNonceA,
+        deadline: 115792089237316195423570985008687907853269984665640564039457584007913129639935n //usar parseInt(Date.now() / 1000) + 600 p.ej
+    }
+
+    const signatureA = await walletClient.signTypedData({
+        account,
+        domain: domainA,
+        types,
+        primaryType: "Permit",
+        message: messageA
+    })
+    const parsedSignatureA = parseSignature(signatureA)
+
+    //hago esto para ahorrar tiempo, hacedlo como lo tengáis
+    const quote = await routerContract.read.quoteAddLiquidity([GT3_ADDRESS, WBTC_ADDRESS, messageA.value, maxUint256])
+
+    const messageB = {
+        owner: account.address,
+        spender: ROUTER_ADDRESS,
+        value: quote[1],
+        nonce: userPermitNonceB,
+        deadline: 115792089237316195423570985008687907853269984665640564039457584007913129639935n //usar parseInt(Date.now() / 1000) + 600 p.ej
+    }
+
+    const signatureB = await walletClient.signTypedData({
+        account,
+        domain: domainB,
+        types,
+        primaryType: "Permit",
+        message: messageB
+    })
+    const parsedSignatureB = parseSignature(signatureB)
+
+    await walletClient.writeContract({
+        address: ROUTER_ADDRESS,
+        abi: UNIVERSAL_ROUTER_ABI,
+        functionName: "addLiquidityWithPermit",
+        args: [
+            [GT3_ADDRESS, WBTC_ADDRESS],
+            [messageA.value, messageB.value],
+            [0n, 0n], //esto no será 0, pero ya lo tendréis calculado, eso sigue igual
+            account.address,
+            messageA.deadline,
+            [
+                { v: parsedSignatureA.v, r: parsedSignatureA.r, s: parsedSignatureA.s },
+                { v: parsedSignatureB.v, r: parsedSignatureB.r, s: parsedSignatureB.s }
+            ]
+        ],
+        account
+    })
+}
+
+main()
